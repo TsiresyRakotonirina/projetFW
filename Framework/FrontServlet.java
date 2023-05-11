359136e598df7c98940abc44c21c73fcfc5460a8
Tsix <tsiresyrakotonirina02@gmail.com>
2023-05-12 02:26:25 +0300

    sprint2

--- Framework/FrontServlet.java
+++ Framework/FrontServlet.java
@@ -1,5 +1,12 @@
 package etu002015.framework.servlet;
 
-public class FrontServlet {
+import java.util.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import etu002015.framework.Mapping;
+
+public class FrontServlet extends HttpServlet {
+
+    HashMap<String,Mapping> MappingUrls;
 
 }
